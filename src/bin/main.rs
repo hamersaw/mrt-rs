570a54283933d23b80ed58d52475009ab2aacd8b
Dan Rammer <hamersaw@gmail.com>
2016-06-30 03:17:41 -0600

    parsing MRTHeader

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,29 @@
+extern crate rustmrt;
+
+use std::fs::File;
+
+use rustmrt::{MRTScanner, MRTType};
+
 fn main() {
-    println!("hello world!");
+    //open reader
+    let file = match File::open("/home/hamersaw/Downloads/updates.20160101.1230") {
+        Ok(file) => file,
+        Err(e) => panic!("{}", e),
+    };
+
+    //open scanner and parse messages
+    let mut scanner = MRTScanner::new(Box::new(file));
+    loop {
+        let mrt_message = match scanner.scan() {
+            Ok(mrt_message) => mrt_message,
+            Err(e) => panic!("{}", e),
+        };
+
+        match mrt_message.mrt_type {
+            MRTType::BGP4MP => {
+                
+            },
+            _ => println!("skipping message"),
+        }
+    }
 }
